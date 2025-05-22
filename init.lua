e12ba778914da563657239ac72c0510b01177e61
monk3yd <jegajardog@gmail.com>
2025-05-22 10:23:09 -0400

    Try format JSON

--- init.lua
+++ init.lua
@@ -802,7 +802,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
       },
     },
   },
