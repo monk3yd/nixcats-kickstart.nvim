1b7cc98059159342d770e89a5ec852caf7dea099
monk3yd <jegajardog@gmail.com>
2025-11-13 10:52:44 -0300

    fix(zellij): explicit hotkeys for movings tabs

--- lua/custom/plugins/zellij-nav.lua
+++ lua/custom/plugins/zellij-nav.lua
@@ -2,12 +2,11 @@ return {
   'swaits/zellij-nav.nvim',
   lazy = true,
   event = 'VeryLazy',
-  -- use default hotkey behaviour
-  -- keys = {
-  --   { '<c-h>', '<cmd>ZellijNavigateLeft<cr>', { silent = true, desc = 'navigate left' } },
-  --   { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
-  --   { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
-  --   { '<c-l>', '<cmd>ZellijNavigateRight<cr>', { silent = true, desc = 'navigate right' } },
-  -- },
+  keys = {
+    { '<c-h>', '<cmd>ZellijNavigateLeftTab<cr>', { silent = true, desc = 'navigate left or tab' } },
+    { '<c-j>', '<cmd>ZellijNavigateDown<cr>', { silent = true, desc = 'navigate down' } },
+    { '<c-k>', '<cmd>ZellijNavigateUp<cr>', { silent = true, desc = 'navigate up' } },
+    { '<c-l>', '<cmd>ZellijNavigateRightTab<cr>', { silent = true, desc = 'navigate right or tab' } },
+  },
   opts = {},
 }
