fc50f047a80bb81e6e872f60a7a86266655a71d8
monk3yd <jegajardog@gmail.com>
2026-01-18 11:52:36 -0300

    fix(opencode): move leader o to leader a for org compatibility

--- lua/custom/plugins/opencode.lua
+++ lua/custom/plugins/opencode.lua
@@ -24,31 +24,31 @@ return {
       require('opencode').ask('@this: ', { submit = true })
     end, { desc = 'opencode: Ask about this' })
 
-    vim.keymap.set({ 'n', 'x' }, '<leader>os', function()
+    vim.keymap.set({ 'n', 'x' }, '<leader>as', function()
       require('opencode').select()
     end, { desc = 'opencode: Select prompt' })
 
-    vim.keymap.set({ 'n', 'x' }, '<leader>o+', function()
+    vim.keymap.set({ 'n', 'x' }, '<leader>a+', function()
       require('opencode').prompt '@this'
     end, { desc = 'opencode: Add this' })
 
-    -- vim.keymap.set('n', '<leader>ot', function()
+    -- vim.keymap.set('n', '<leader>at', function()
     --   require('opencode').toggle()
     -- end, { desc = 'opencode: Toggle embedded' })
 
-    vim.keymap.set('n', '<leader>oc', function()
+    vim.keymap.set('n', '<leader>ac', function()
       require('opencode').command()
     end, { desc = 'opencode: Select command' })
 
-    vim.keymap.set('n', '<leader>on', function()
+    vim.keymap.set('n', '<leader>an', function()
       require('opencode').command 'session_new'
     end, { desc = 'opencode: New session' })
 
-    vim.keymap.set('n', '<leader>oi', function()
+    vim.keymap.set('n', '<leader>ai', function()
       require('opencode').command 'session_interrupt'
     end, { desc = 'opencode: Interrupt session' })
 
-    vim.keymap.set('n', '<leader>oA', function()
+    vim.keymap.set('n', '<leader>aA', function()
       require('opencode').command 'agent_cycle'
     end, { desc = 'opencode: Cycle selected agent' })
 
